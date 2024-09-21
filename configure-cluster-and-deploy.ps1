6ed330d95f4564db9531d4fae6eb04597f8adc61
Joseph Whiteaker <josephsims1@gmail.com>
2024-09-21 04:55:15 -0400

    move the script up in front of the sleeping

--- configure-cluster-and-deploy.ps1
+++ configure-cluster-and-deploy.ps1
@@ -10,7 +10,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 # Wait for the cluster to be ready
 kubectl wait --for=condition=Ready node --all --timeout=5m
 
-echo "Sleep for a bit to let the cluster settle"
+Write-Output "Sleep for a bit to let the cluster settle"
 Start-Sleep -Seconds 10
 
 
@@ -20,15 +20,17 @@ helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace -f .\arg
 
 # Wait for the ArgoCD pods to be ready
 kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=5m
-echo "Sleep for a bit to let the ArgoCD settle"
-Start-Sleep -Seconds 20
+Write-Output "Sleep for a bit to let the ArgoCD settle"
+
 
 
 # Capture the secret from kubectl output
 $secret = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
 $decodedSecret = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($secret))
-Write-Output "ArgoCD Initial Admin Password: $decodedSecret"
+Write-Output "`n`n`nArgoCD Initial Admin Password: $decodedSecret`n`n`nArgo Url: http://localhost:30080`n`n"
 
+# Sleep for a bit to let the ArgoCD settle into the cluster
+Start-Sleep -Seconds 20
 
 
 # Install the Bootstrap Helm Chart
