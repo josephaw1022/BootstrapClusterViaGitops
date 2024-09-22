56f8669ebc4b2eee3583d4132b1738b55301aceb
Joseph Whiteaker <josephsims1@gmail.com>
2024-09-22 17:11:44 -0400

    two new scripts for conveinence

---
+++ upgrade-bootstrap-chart.ps1
@@ -0,0 +1,4 @@
+
+
+# Upgrade the Bootstrap Helm Chart
+helm upgrade --install bootstrap ./BootstrapCluster -n bootstrap-cluster --create-namespace
---
+++ upgrade-test-ingress-chart.ps1
@@ -0,0 +1,3 @@
+# Upgrade the Test Helm Chart
+Write-Output "Installing the Test Helm Chart"
+helm upgrade --install test-ingress ./TestIngress
